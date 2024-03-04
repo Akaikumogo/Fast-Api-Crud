a052f6972cd106d876d6e738f4f6d930e124c351
Akaikumogo <98486553+Akaikumogo@users.noreply.github.com>
2024-03-04 13:12:40 +0500

    done

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
-from typing import Optional
+from fastapi import Depends, FastAPI, HTTPException
+import uvicorn
 
-from fastapi import FastAPI
-
+from database import engine,SessionLocal
+from models import Base, User
+from schemas import UserSchema, UserUpdateSchema
+from sqlalchemy.orm import Session
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
+def get_db():
+    try :
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+@app.post("/addUser")
+async def add_user(req: UserSchema, db: Session = Depends(get_db)):
+    user = User(name=req.name, email=req.email, nickname=req.nickname)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return {"status_code": 200, "message": "Successfully added user"}
 
+@app.get("/Users/{user_id}")
+async def getUser(user_id:int,db: Session=Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user :
+        return user
+    return HTTPException(status_code=404,detail="User not found")
+@app.get("/AllUsers")
+async def allUsers(db:Session=Depends(get_db)):
+    users = db.query(User).all()
+    return users
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+from fastapi import HTTPException
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.delete("/Users/{user_id}")
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return {"message": "User deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+@app.put("/Users/{user_id}")
+async def edit_user(user_id: int, user_update_data: UserUpdateSchema, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    user.name = user_update_data.name
+    user.email = user_update_data.email
+    user.nickname = user_update_data.nickname
+    db.commit()
+    return {"message": "User updated successfully"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
